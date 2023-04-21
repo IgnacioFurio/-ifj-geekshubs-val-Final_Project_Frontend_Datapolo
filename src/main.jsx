db3d48002a513195337bbcce6db5b5ecc0b700f0
Ignacio Furió José <bichoifj@gmail.com>
2023-04-21 10:39:02 +0200

    feat: add readux and redux persistor to all my app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//redux
+import { Provider } from 'react-redux'
+import store from './app/store';
+//redux persistor
+import { PersistGate } from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore';
+
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+
+const persistor = persistStore
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 )
