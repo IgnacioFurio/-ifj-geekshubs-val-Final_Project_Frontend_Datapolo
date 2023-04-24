239f2e5eb26a8f7334e4b0716bb5d803fbfea502
Ignacio Furió José <bichoifj@gmail.com>
2023-04-24 13:53:02 +0200

    feat: add validations to Input fields in the SignUp page

--- src/common/Input/Input.jsx
+++ src/common/Input/Input.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
+import './Input.css'
 
 
 export const Input = ({className, type, name, placeholder, required, error, changeFunction, blurFunction}) => {
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+//helper
+import { validate } from '../../helpers/useful';
+//render
 import Form from 'react-bootstrap/Form';
 import { Input } from '../../common/Input/Input';
+import './SignUp.css';
 
 export const SignUp = () => {
 
@@ -9,12 +13,35 @@ export const SignUp = () => {
     //set data for the new user
     const [newUser, setNewUser] = useState(
         {
-            name: '',
+            username: '',
             email: '',
             password: '',
             password2: ''
         }
     );
+
+    //validate the value inside the inputs
+    const [validInputField, setValidInputfield] = useState(
+        {
+            usernameValid: '',
+            emailValid: '',
+            passwordValid: '',
+            password2Valid: ''
+        }
+    );
+
+    //error messages if something is wrong inside the inputs
+    const [errorInputField, setErrorInputField] = useState(
+        {
+            usernameError: '',
+            emailError: '',
+            passwordError: '',
+            password2Error: ''
+        }
+    );
+
+    //activate submit button
+    const [submitActive, setSubmitActive] = useState(false);
 
     //HANDLER
     const inputHandler = (e) => {
@@ -31,45 +58,180 @@ export const SignUp = () => {
     //USEEFFECT
     useEffect(()=>{
         console.log(newUser);
-    },[newUser])
+        console.log(validInputField);
+        console.log(errorInputField);
+        console.log(submitActive);
+
+    })
+
+    useEffect(() => {
+        //functions to make submit button activated
+        //in case that a field is empty
+        for(let empty in newUser){
+            
+            if(newUser[empty] === ""){
+        
+                setSubmitActive(false);
+                
+                return;
+                };
+        };
+    
+        //in case that a field is not valid
+        for(let valid in validInputField){
+    
+            if(validInputField[valid] === false){
+        
+                setSubmitActive(false);
+                return;
+            };
+        };
+        
+        //in case that a field shows an error
+        for(let error in errorInputField){
+    
+            if(errorInputField[error]){
+                
+                setSubmitActive(false);
+                return;
+                };
+        };
+        
+        //in case the data it's full validated
+        setSubmitActive(true);
+    });
+
+    //FUNCTIONS
+    const checkError = (e) => {
+        
+        let error = "";
+    
+        let check = validate(
+            e.target.name,
+            e.target.value,
+            e.target.required
+            );
+            
+        error = check.message
+    
+        setValidInputfield((prevState) => (
+                {
+                    ...prevState,
+                    [e.target.name + 'Valid']: check.valid
+                }
+            )
+        );
+    
+        setErrorInputField((prevState) => (
+                {
+                ...prevState,
+                [e.target.name + 'Error']: error
+                }
+            )
+        );
+    };
+
+    //confirm password function
+    const confirmPass = (e) => {
+
+        if(newUser.password2 === ''){
+
+            setValidInputfield((prevState) => (
+                {
+                    ...prevState,
+                    [e.target.name + 'Valid']: false
+                }
+                )
+            );
+    
+            setErrorInputField((prevState) => (
+                {
+                    ...prevState,  
+                    [e.target.name + 'Error']: "Field 'Confirm Password' required."
+                }
+                )
+            );
+
+        }else if(newUser.password2 !== newUser.password && newUser.password2 !== ""){
+
+        setValidInputfield((prevState) => (
+            {
+                ...prevState,
+                [e.target.name + 'Valid']: false
+            }
+            )
+        );
+
+        setErrorInputField((prevState) => (
+            {
+                ...prevState,  
+                [e.target.name + 'Error']: 'Password and Confirm Password shoud be the same.'
+            }
+            )
+        );
+
+        } else {
+
+        setValidInputfield((prevState) => (
+            {
+                ...prevState,
+                [e.target.name + 'Valid']: true
+            }
+            )
+        );
+
+        setErrorInputField((prevState) => (
+            {
+                ...prevState,  
+                [e.target.name + 'Error']: ''
+            }
+            )
+        );
+
+        };
+    }
 
     return (
         <>
             <Input
-                className={'shadowBox'}
+                className={errorInputField.usernameError === '' ? 'shadowBox' : 'shadowBoxError'}
                 type={'text'}
-                name={'name'}
+                name={'username'}
                 placeholder={'CDW Turia'}
                 required={true}
+                error={errorInputField.usernameError}
                 changeFunction={(e)=>inputHandler(e)}
-                blurFunction={()=>{}}
+                blurFunction={(e)=>checkError(e)}
             />
             <Input
-                className={'shadowBox'}
+                className={errorInputField.emailError === '' ? 'shadowBox' : 'shadowBoxError'}
                 type={'email'}
                 name={'email'}
                 placeholder={'cdwturia@email.com'}
                 required={true}
+                error={errorInputField.emailError}
                 changeFunction={(e)=>inputHandler(e)}
-                blurFunction={()=>{}}
+                blurFunction={(e)=>checkError(e)}
             />
             <Input
-                className={'shadowBox'}
+                className={errorInputField.passwordError === '' ? 'shadowBox' : 'shadowBoxError'}
                 type={'password'}
                 name={'password'}
                 placeholder={'1234567W'}
                 required={true}
+                error={errorInputField.passwordError}
                 changeFunction={(e)=>inputHandler(e)}
-                blurFunction={()=>{}}
+                blurFunction={(e)=>checkError(e)}
             />
             <Input
-                className={'shadowBox'}
+                className={errorInputField.password2Error === '' ? 'shadowBox' : 'shadowBoxError'}
                 type={'password'}
                 name={'password2'}
                 placeholder={'1234567W'}
                 required={true}
+                error={errorInputField.password2Error}
                 changeFunction={(e)=>inputHandler(e)}
-                blurFunction={()=>{}}
+                blurFunction={(e)=>confirmPass(e)}
             />
         </>
     )
