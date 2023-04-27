514245951afc6e64f29a76b3db1cf2f680e84888
Ignacio Furió José <bichoifj@gmail.com>
2023-04-27 12:19:23 +0200

    fix: error that did not stop the apicall when there was no data registered by the user

--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -93,13 +93,26 @@ export const Teams = () => {
 
             dispatch(reload({updatedData: {}}))
 
-            getAllMyTeams(userDataRdx.userCredentials.user.id ,userDataRdx.userCredentials.token)
-                .then(
-                    result => {
-                        setTeamData(result.data.data)
-                        }
-                )
-                .catch(error => console.log(error));
+            try {
+                setTimeout(() => {
+                
+                    getAllMyTeams(userDataRdx.userCredentials.user.id ,userDataRdx.userCredentials.token)
+                        .then(
+                            result => {
+                                console.log(result.data.data);
+                                result.data.data ? setTeamData(result.data.data) : ''                         
+                            }
+                            
+                            )
+                            .catch(error => console.log(error));
+                            
+                        }, 4000)
+            } catch (error) {
+                
+                setShowAddteam(true)
+            }
+            
+            
 
         };
 
@@ -165,101 +178,102 @@ export const Teams = () => {
 
     return (
         <>
-            {
-                teamData.length === 0 ? (
-                    <>
-                        <Container fluid>
-                            <img src={spiner} className="spinnerDesign m-5" alt="spinner"/>
-                            <h3 className='font fw-bold'>Looking for your information.</h3>
-                        </Container>
-                    </>
-                ) : (
-                    <>
-                        <Container>
-                            <Row className='mt-5 mb-3'>
-                                <Col xs={9} className='d-flex justify-content-start '>
-                                <h2 className='font fw-bold'>Your Teams</h2>
-                                </Col>
-                                <Col xs={2} className='d-flex justify-content-end fw-bold text-primary'>
-                                    <p>Add team </p>
-                                </Col>
-                                <Col xs={1}>
-                                    <img src={add} className="updateIcon" alt="GitHub" onClick={() => handleAddteamShow()}/>
-                                </Col>
-                                <hr className='font fw-bold'></hr>
-                            </Row>
-                            <Row>
-                                {teamData.map(data =>
-                                        {
-                                            return <TableTeams key={data.id} id={data.id} teamName={data.team_name}/>
+        <Container>
+            <Row className='mt-5 mb-3'>
+                <Col xs={9} className='d-flex justify-content-start '>
+                <h2 className='font fw-bold'>Your Teams</h2>
+                </Col>
+                <Col xs={2} className='d-flex justify-content-end fw-bold text-primary'>
+                    <p>Add team </p>
+                </Col>
+                <Col xs={1}>
+                    <img src={add} className="updateIcon" alt="addIcon" onClick={() => handleAddteamShow()}/>
+                </Col>
+                <hr className='font fw-bold'></hr>
+            </Row>
+                {
+                    teamData.length === 0 ? (
+                        <>
+                            <Container fluid>
+                                <img src={spiner} className="spinnerDesign m-5" alt="spinner"/>
+                                <h3 className='font fw-bold'>Looking for your information.</h3>
+                            </Container>
+                        </>
+                    ) : (
+                        <>
+                            <Container>                            
+                                <Row>
+                                    {teamData.map(data =>
+                                            {
+                                                return <TableTeams key={data.id} id={data.id} teamName={data.team_name}/>
+                                            }
+                                            )
                                         }
+                                </Row>
+                            </Container>
+                            
+                        </>
+                    )
+                }
+                {
+                    message === '' ? (
+                        <Modal show={showAddTeam} onHide={() => handleAddTeamClose()}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Add a new team for your data.</Modal.Title>
+                                </Modal.Header>                        
+                                    <Modal.Body>
+                                        <Input
+                                            className={''}
+                                            type={'text'}
+                                            name={'new_team'}
+                                            placeholder={'Type the new team name here'}
+                                            required={true}
+                                            error={errorInputField}
+                                            changeFunction={((e)=>inputHandler(e))}
+                                            blurFunction={(e)=>checkError(e)}
+                                            />
+                                    </Modal.Body>        
+                                <Modal.Footer>
+                                    <Button variant="danger" onClick={() => handleUpdateClose()}>
+                                        Cancel Changes
+                                    </Button>
+                                    {
+                                        activeSubmit ? (
+                                            <Button variant="success" onClick={() => createTeam()}>
+                                                Save Changes
+                                            </Button>
+                                        ) : (
+                                            <Button variant="secondary">
+                                                Save Changes
+                                            </Button>
                                         )
                                     }
-                            </Row>
-                        </Container>
-                        {
-                            message === '' ? (
-                                    <Modal show={showAddTeam} onHide={() => handleAddTeamClose()}>
-                                        <Modal.Header closeButton>
-                                            <Modal.Title>Add a new team for your data.</Modal.Title>
-                                        </Modal.Header>                        
-                                            <Modal.Body>
-                                                <Input
-                                                    className={''}
-                                                    type={'text'}
-                                                    name={'new_team'}
-                                                    placeholder={'Type the new team name here'}
-                                                    required={true}
-                                                    error={errorInputField}
-                                                    changeFunction={((e)=>inputHandler(e))}
-                                                    blurFunction={(e)=>checkError(e)}
-                                                    />
-                                            </Modal.Body>        
-                                        <Modal.Footer>
-                                            <Button variant="danger" onClick={() => handleUpdateClose()}>
-                                                Cancel Changes
-                                            </Button>
-                                            {
-                                                activeSubmit ? (
-                                                    <Button variant="success" onClick={() => createTeam()}>
-                                                        Save Changes
-                                                    </Button>
-                                                ) : (
-                                                    <Button variant="secondary">
-                                                        Save Changes
-                                                    </Button>
-                                                )
-                                            }
-                                        </Modal.Footer>
-                                    </Modal>
-                                ) : (
-                                    <Modal show={showAddTeam} onHide={() => handleAddTeamClose()}>
-                                        <Modal.Header closeButton>
-                                            <Modal.Title>Add a new team for your data.</Modal.Title>
-                                        </Modal.Header>                        
-                                            <Modal.Body>
-                                                <Input
-                                                    className={''}
-                                                    type={'text'}
-                                                    name={'new_team'}
-                                                    placeholder={'Type the new team name here'}
-                                                    required={true}
-                                                    error={errorInputField}
-                                                    changeFunction={((e)=>inputHandler(e))}
-                                                    blurFunction={(e)=>checkError(e)}
-                                                    />
-                                            </Modal.Body>        
-                                        <Modal.Footer>
-                                        <h4 className='d-flex justify-content-center font fw-bold'>{message}</h4>
-                                        </Modal.Footer>
-                                    </Modal>
-                                )
-                        }
-                        
-                    </>
-                )
-            }
-            
+                                </Modal.Footer>
+                            </Modal>
+                        ) : (
+                            <Modal show={showAddTeam} onHide={() => handleAddTeamClose()}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Add a new team for your data.</Modal.Title>
+                                </Modal.Header>                        
+                                    <Modal.Body>
+                                        <Input
+                                            className={''}
+                                            type={'text'}
+                                            name={'new_team'}
+                                            placeholder={'Type the new team name here'}
+                                            required={true}
+                                            error={errorInputField}
+                                            changeFunction={((e)=>inputHandler(e))}
+                                            blurFunction={(e)=>checkError(e)}
+                                            />
+                                    </Modal.Body>        
+                                <Modal.Footer>
+                                <h4 className='d-flex justify-content-center font fw-bold'>{message}</h4>
+                                </Modal.Footer>
+                            </Modal>
+                        )
+                }
+            </Container>            
         </>
     )
 }
