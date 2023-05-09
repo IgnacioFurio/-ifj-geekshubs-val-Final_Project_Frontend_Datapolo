2aa39144380b44c82d07b9acdb073e81ed4c2063
Ignacio Furió José <bichoifj@gmail.com>
2023-05-10 01:21:19 +0200

    feat: now you can see where the goals came from in the information modal about the game at Game page

--- src/common/TableGoalsInfo/TableGoalsInfo.jsx
+++ src/common/TableGoalsInfo/TableGoalsInfo.jsx
@@ -5,17 +5,24 @@ import { getAllMyGoalsByTeamIdAndGameId } from '../../services/apiCalls';
 import { userData } from '../../pages/Slices/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 //render
+import { Modal } from 'bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import info from '../../assets/info.png';
 import update from '../../assets/actualizar-flecha.png';
 import del from '../../assets/borrar.png';
+import hide from '../../assets/esconder.png';
+import goal from '../../assets/jugador-de-waterpolo-con-las-bolas-en-el-agua.png';
+
 
 
 export const TableGoalsInfo = ({goalData, playersData}) => {
 
+    //HOOKS
     const [playerName, setPlayerName] = useState('');
+
+    const [showInfo, setShowInfo] = useState(false);
 
     useEffect(() => {
 
@@ -26,21 +33,81 @@ export const TableGoalsInfo = ({goalData, playersData}) => {
             }
         }
     }, [playerName]);
-
-    useEffect(()=>{
-        console.log(playerName);
-    });
     
-    // handleInfoShow
+    //HANDLER
+    const handleInfoShow = () => {
+
+        setShowInfo(true)
+
+    }
+    
+    const handleInfoClose = () => {
+
+        setShowInfo(false)
+
+    }
+    
     return (
         <> 
             <Row>
                 <Col xs={5}>{playerName}</Col>
                 <Col xs={4}>{goalData.player_nº}</Col>
-                <Col xs={1}><img src={info} alt="update" className='infoIcon' onClick={() => {}}/></Col>                    
+                <Col xs={1}><img src={info} alt="update" className='infoIcon' onClick={() => handleInfoShow()}/></Col>                    
                 <Col xs={1}><img src={update} alt="update" className='updateIcon' onClick={() => {}}/></Col>                    
                 <Col xs={1}><img src={del} alt="delete" className='deleteIcon' onClick={() => {}}/></Col>
             </Row>
+            {
+                showInfo ? (
+                    <>
+                        <Row className='my-3 d-flex align-items-center'>
+                            <Col xs={2}></Col>
+                            <Col className='font fw-bold text-center mt-2'>Zone where the goal came from.</Col>
+                            <Col xs={2} className='d-flex justify-content-end'><img src={hide} alt="hide" className='updateIcon' onClick={() => handleInfoClose()}/></Col>
+                        </Row>
+                        <Row>
+                            <Col></Col>
+                            <Col><div className='goal'></div></Col>
+                            <Col></Col>
+                        </Row>
+                        <Row className='water'>
+                            <Col xs={4} className='field1 d-flex justify-content-center align-items-center'>
+                            {goalData.zone == 1 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                            <Col xs={4} className='field2 d-flex justify-content-center align-items-center'> 
+                                
+                                {goalData.zone == 2 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                            <Col xs={4} className='field3 d-flex justify-content-center align-items-center'>
+                                {goalData.zone == 3 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                        </Row>
+                        <Row className='water'>
+                            <Col xs={4} className='field4 d-flex justify-content-center align-items-center'>
+                                {goalData.zone == 4 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                            <Col xs={4} className='field5 d-flex justify-content-center align-items-center'>
+                                {goalData.zone == 5 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                            <Col xs={4} className='field6 d-flex justify-content-center align-items-center'>
+                                {goalData.zone == 6 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                        </Row>
+                        <Row className='water'>
+                            <Col xs={4} className='field7 d-flex justify-content-center align-items-center'>
+                                {goalData.zone == 7 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                            <Col xs={4} className='field8 d-flex justify-content-center align-items-center'>
+                                {goalData.zone == 8 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                            <Col xs={4} className='field9 d-flex justify-content-center align-items-center'>
+                                {goalData.zone == 9 ? (<img src={goal} alt="goal" className='goalIcon'/>) : ('')}
+                            </Col>
+                        </Row>
+                    </>
+                ) : (
+                    ''
+                )
+            }            
         </>
     )
 }
